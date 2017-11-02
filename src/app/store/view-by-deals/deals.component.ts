20399e8cada46f89a04686aad12c5efaf3812b32
omprakash-sharma <ops.prakash90@gmail.com>
2017-11-02 18:35:46 +0530

    deal&promotion screen modifyed.

--- src/app/store/view-by-deals/deals.component.ts
+++ src/app/store/view-by-deals/deals.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deals.component.css']
 })
 export class DealsComponent implements OnInit {
-
+  // initial variables
+  navListItem:[any];
   constructor() { }
 
   ngOnInit() {
+    this.navListItem = [{
+      dealName:"All deals",
+      desc:"all deals",
+      active:true
+    },{
+      dealName:"Deal of the Day",
+      desc:"today deals & offers",
+      active:false
+    },{
+      dealName:"Saving & Sales",
+      desc:"saving and sales",
+      active:false
+    },{
+      dealName:"Coupons & Vouchers",
+      desc:"coupons and vouchers",
+      active:false
+    },{
+      dealName:"Combo & Bundles",
+      desc:"combo and bundles",
+      active:false
+    }];
   }
 
 }
